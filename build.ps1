233c8c2c097d409c350b633b12fb162cb5d144b4
Kevin Jones <kevin@vcsjones.com>
2017-04-28 22:37:18 -0400

    Fix paths when script moved.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
-dotnet restore $PSScriptRoot\..
+dotnet restore $PSScriptRoot
 If ($lastexitcode -ne 0) {
 	Write-Host "Restore failed."
 	exit 1
 }
-dotnet test $PSScriptRoot\..\tests\OpenVsixSignTool.Core.Tests\OpenVsixSignTool.Core.Tests.csproj
+dotnet test $PSScriptRoot\tests\OpenVsixSignTool.Core.Tests\OpenVsixSignTool.Core.Tests.csproj
 
 If ($lastexitcode -ne 0) {
 	Write-Host "Tests failed."
