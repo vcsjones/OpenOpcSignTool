0c357fd0ba1174e578441c833b7a20be03cd2d3a
Kevin Jones <kevin@vcsjones.com>
2017-09-12 19:18:22 -0400

    Specific framework tests.

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@ If ($lastexitcode -ne 0) {
 	Write-Host "Restore failed."
 	exit 1
 }
-dotnet test $PSScriptRoot\tests\OpenVsixSignTool.Core.Tests\OpenVsixSignTool.Core.Tests.csproj
+dotnet test --framework net462 $PSScriptRoot\tests\OpenVsixSignTool.Core.Tests\OpenVsixSignTool.Core.Tests.csproj
 $CoreTestsFailed = $lastexitcode
 
-dotnet test $PSScriptRoot\tests\OpenVsixSignTool.Tests\OpenVsixSignTool.Tests.csproj
+dotnet test --framework net462 $PSScriptRoot\tests\OpenVsixSignTool.Tests\OpenVsixSignTool.Tests.csproj
 $IntegrationTestsFailed = $lastexitcode
 
 If (($CoreTestsFailed -ne 0) -or ($IntegrationTestsFailed -ne 0)) {
