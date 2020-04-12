2b36d0ae8632ee1cad79041617343c82727f5ab2
Scott Xu <scott-xu@msn.com>
2020-04-12 11:26:31 +0800

    Update Program.cs
    update the option description

--- src/OpenVsixSignTool/Program.cs
+++ src/OpenVsixSignTool/Program.cs
@@ -11,12 +11,12 @@ namespace OpenVsixSignTool
                 {
                     signConfiguration.Description = "Signs a VSIX package.";
                     signConfiguration.HelpOption("-? | -h | --help");
-                    var sha1 = signConfiguration.Option("-s | --sha1", "A hex-encoded SHA-1 thumbprint of the certificate used to sign the executable.", CommandOptionType.SingleValue);
+                    var sha1 = signConfiguration.Option("-s | --sha1", "A hex-encoded SHA-1 thumbprint of the certificate used to sign the opc file.", CommandOptionType.SingleValue);
                     var pfxPath = signConfiguration.Option("-c | --certificate", "A path to a PFX file to perform the signature.", CommandOptionType.SingleValue);
                     var password = signConfiguration.Option("-p | --password", "The password for the PFX file.", CommandOptionType.SingleValue);
                     var timestamp = signConfiguration.Option("-t | --timestamp", "A URL of the timestamping server to timestamp the signature.", CommandOptionType.SingleValue);
                     var timestampAlgorithm = signConfiguration.Option("-ta | --timestamp-algorithm", "The digest algorithm of the timestamp.", CommandOptionType.SingleValue);
-                    var fileDigest = signConfiguration.Option("-fd | --file-digest", "A URL of the timestamping server to timestamp the signature.", CommandOptionType.SingleValue);
+                    var fileDigest = signConfiguration.Option("-fd | --file-digest", "The digest algorithm to hash the opc file with.", CommandOptionType.SingleValue);
                     var force = signConfiguration.Option("-f | --force", "Force the signature by overwriting any existing signatures.", CommandOptionType.NoValue);
                     var file = signConfiguration.Argument("file", "A to the VSIX file.");
 
